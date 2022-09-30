43aa7e5a6188a0d02db86664502355107f3c203a
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-09-30 18:06:53 -0300

    corrigindo arquivo getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
-const getAnimalsOlderThan = require('./getAnimalsOlderThan');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+
 }
 
 module.exports = getAnimalsOlderThan;
