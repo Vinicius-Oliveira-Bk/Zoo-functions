fced6c8891b9c5dc2ce2e30d2905d2b3b7195001
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-03 17:01:59 -0300

    adicionando requisito: 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-
+  const test = species.filter((element) => element.name === animal);
+  return test[0].residents.every((resident) => resident.age >= age);
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(...employeeName) {
   // seu código aqui
 }
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
+
+/* const listEmployee = [{}];
+employeeName.forEach((name) => listEmployee.push(employees.find((ob) => ob.firstName === name)));
+if (listEmployee.length !== 0) {
+  return listEmployee; */
