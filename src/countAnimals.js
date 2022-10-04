17ac41ca4bfdf3784f952b2a5bac041c5e49f904
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-04 18:18:33 -0300

    adicionando requisito: 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const animals = {};
+    species.forEach((element) => {
+      animals[element.name] = element.residents.length;
+    });
+    return animals;
+  } if (!animal.sex) {
+    return species.filter(({ name: nome }) => animal.specie === nome)
+      .map(({ residents }) => residents.length)[0];
+  } return species.filter(({ name: nome }) => animal.specie === nome)
+    .map(({ residents }, index) => residents[index].sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,11 +3,18 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  return employees;
+  return employees.some((manager) => manager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const manager = isManager(managerId);
+  const responsibilities = [];
+  if (manager === true) {
+    employees.filter((element) => element.managers.includes(managerId))
+      .filter((element) => responsibilities.push(`${element.firstName} ${element.lastName}`));
+    return responsibilities;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
