cbf6a69cc9337b895f6a4eceb5f80754e8f3726b
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-05 20:02:07 -0300

    adicionando requisito: 7

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,6 @@
 const getOpeningHours = require('../src/getOpeningHours');
+const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+
+});
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -18,4 +18,31 @@ describe('Testes da função HandlerElephants', () => {
     const expected = 10.5;
     expect(actual).toEqual(expected);
   });
+
+  it('A função é case sensitive;', () => {
+    const actual = handlerElephants('averageage');
+    expect(actual).toBeNull();
+  });
+
+  it('Para o argumento location deve retornar a string NW', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toContain(expected);
+  });
+
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    const actual = handlerElephants('popularity');
+    expect(actual).toBeGreaterThanOrEqual(5);
+  });
+
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    const expected = ['Monday'];
+    expect(['Friday', 'Saturday', 'Sunday', 'Tuesday']).toEqual(
+      expect.not.arrayContaining(expected),
+    );
+  });
+
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
