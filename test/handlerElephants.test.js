7951566a6bb895e96dbc22997281755c300ebbf5
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-05 21:03:38 -0300

    adicionando requisito: 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -45,4 +45,13 @@ describe('Testes da função HandlerElephants', () => {
   it('Não passando argumentos a função deve retornar undefined', () => {
     expect(handlerElephants()).toBeUndefined();
   });
+
+  it('Passando por argumento um objeto vazio ({}) deve retornar a string "Parâmetro inválido, é necessário uma string"', () => {
+    const actual = handlerElephants({});
+    expect(actual).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Passada uma string que não contempla uma funcionalidade deve retornar null', () => {
+    expect(handlerElephants('xablau')).toBeNull();
+  });
 });
