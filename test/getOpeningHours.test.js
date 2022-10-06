1cced33182a84ad847bbd3f1a7ba9ef1cc735201
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-05 21:24:46 -0300

    adicionando requisito: 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,6 +1,33 @@
 const getOpeningHours = require('../src/getOpeningHours');
-const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função getOpeningHours', () => {
+  it('Não passando argumentos. Deverá retornar o objeto:', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
 
+  it('Para os argumentos Monday e 09:00-AM deve retornar a string "The zoo is closed"', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Para os argumentos Tuesday e 09:00-AM deve retornar a string "The zoo is open"', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Para os argumentos Wednesday e 09:00-PM deve retornar a string "The zoo is closed"', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
 });
