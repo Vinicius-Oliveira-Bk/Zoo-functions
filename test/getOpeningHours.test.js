37f77c913ea1474a247d472a48c30383980fe84b
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-07 22:25:26 -0300

    adicionando requisito: 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -46,4 +46,12 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: "The minutes should represent a number"', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow();
   });
+
+  it('Para os argumentos Monday e 13:00-AM deve lançar uma exceção com a mensagem: "The hour must be between 0 and 12"', () => {
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow();
+  });
+
+  it('Para os argumentos Tuesday e 09:60-AM deve lançar uma exceção com a mensagem: "The minutes must be between 0 and 59"', () => {
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow();
+  });
 });
