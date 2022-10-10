0ac031a08cb12d5ef23005ee53ea906532b9d0ca
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-10 20:54:19 -0300

    adicionando requisito: 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,12 +1,18 @@
-const { employees } = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(idEmployee) {
   // seu código aqui
   const encontraId = employees.find((element) => idEmployee === element.id);
-  return encontraId;
+  const { responsibleFor } = encontraId;
+  const encontraAnimal = species.find((element) => responsibleFor[0] === element.id);
+  const { residents } = encontraAnimal;
+  const animalByAge = residents.sort((a, b) => a.age - b.age);
+  const arrayAnimal = [`${animalByAge.at(-1).name}`,
+    `${animalByAge.at(-1).sex}`, animalByAge.at(-1).age];
+  return arrayAnimal;
 }
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
