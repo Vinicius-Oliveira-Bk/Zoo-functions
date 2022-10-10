9b1b56e9e098646ba892dba112dd777717bb15bf
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-10-09 23:51:54 -0300

    adicionando requisito: 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,11 +2,40 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const visitantes = {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      visitantes.child += 1;
+    } if (element.age >= 18 && element.age < 50) {
+      visitantes.adult += 1;
+    } if (element.age >= 50) {
+      visitantes.senior += 1;
+    }
+  });
+  return visitantes;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
-
+  let valorEntrada = 0;
+  if (entrants === undefined) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const quantVisitantes = countEntrants(entrants);
+  const { child } = quantVisitantes;
+  const { adult } = quantVisitantes;
+  const { senior } = quantVisitantes;
+  valorEntrada += (adult * 49.99);
+  valorEntrada += (senior * 24.99);
+  valorEntrada += (child * 20.99);
+  return valorEntrada;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -12,9 +12,14 @@ function countAnimals(animal) {
   } if (!animal.sex) {
     return species.filter(({ name: nome }) => animal.specie === nome)
       .map(({ residents }) => residents.length)[0];
-  } return species.filter((element, index) => element[index.name] === animal.specie)
-    .filter((element, index) => animal.sex === element[index.residents[index.sex]]).length;
+  }
+  return species.filter(({ name: nome }) => animal.specie === nome)
+    .map(({ residents }) => residents.sex)[0].length
+    && species.filter(({ sex: sexo }) => animal.sex === sexo).map;
 }
+
+/* species.filter((element, index) => element[index.name] === animal.specie)
+    .filter((element, index) => animal.sex === element[index.residents[index.sex]]).length; */
 
 console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
